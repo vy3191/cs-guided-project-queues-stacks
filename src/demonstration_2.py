3c68b8aa6bea03ff32dc5f2ba52c5b7517b45ae1
vy3191 <venkyafhs@gmail.com>
2021-01-06 18:47:17 -0500

    Completed demonstration_2.py file

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -23,12 +23,37 @@ class Stack:
 class QueueTwoStacks:
     def __init__(self):
         # Your code here
-
+        self.input_stack = Stack()
+        self.output_stack = Stack()
         
     def enqueue(self, item):
         # Your code here
-
+        self.input_stack.push(item)
 
     def dequeue(self):
-        # Your code here
+        # we are going to return the values from the output stack
+        # if the output stack is empty, fill it with values from the input stack
+        if len(self.output_stack.data) == 0:
+            while len(self.input_stack.data) > 0:
+                # pop from the input_stack and push onto the output_stack
+                current_item = self.input_stack.pop()
+                self.output_stack.push(current_item)
+                
+        return self.output_stack.pop()  
+    
+    
+my_queue = QueueTwoStacks()
+my_queue.enqueue(1)
+my_queue.enqueue(2)
+my_queue.enqueue(3)
+my_queue.enqueue(4)
+print(f"The first item added was {my_queue.dequeue()}")
+print(f"The second item added was {my_queue.dequeue()}")
+my_queue.enqueue("E")
+print(f"The third item added was {my_queue.dequeue()}")
+print(f"The fourth item added was {my_queue.dequeue()}")
+print(f"The fifth item added was {my_queue.dequeue()}")       
+                
+                
+        
 
